e71a3e3da2dfb33420dfe53c58ed594049d52d35
Matt Cooper <vtbassmatt@gmail.com>
2026-05-29 07:52:08 -0400

    some UI

--- src/mtgauto.js
+++ src/mtgauto.js
@@ -1,1 +1,7 @@
-console.log("hi")
+(function () {
+  const resultsContainer = document.getElementById("results");
+  const searchBox = document.getElementById("searchBox");
+  searchBox.addEventListener("input", function(ev) {
+    resultsContainer.innerText = ev.target.value;
+  });
+})();
