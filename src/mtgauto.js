c8727ef914b491359429ba3416beb1a73e6ac573
Matt Cooper <vtbassmatt@gmail.com>
2026-05-30 12:48:59 -0400

    Trie: insert and contains

--- src/mtgauto.js
+++ src/mtgauto.js
@@ -1,3 +1,57 @@
+class TrieNode {
+  constructor(contents) {
+    this.contents = contents;
+    this.children = new Map();
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode(null);
+  }
+
+  insert(str) {
+    const addr = this.#toAddr(str);
+
+    let node = this.root;
+    for (const a of addr) {
+      if (!node.children.has(a)) { node.children.set(a, new TrieNode(null)); }
+      node = node.children.get(a);
+    }
+
+    if (node.contents == null) { node.contents = str; }
+    else if (node.contents === str) { return; } // exact duplicates are OK
+    else { throw Error(`"${node.contents}" already tracked and is not equal to "${str}"`); }
+  }
+
+  contains(str) {
+    const addr = this.#toAddr(str);
+
+    let node = this.root;
+    for (const a of addr) {
+      if (node.children.has(a)) { node = node.children.get(a); }
+      else { return false; }
+    }
+
+    return !(node.contents == null);
+  }
+
+  candidates(str) {
+    //
+  }
+
+  #toAddr(str) {
+    const splitter = /\s+/;
+    const addr = [];
+    for (const cp of str.split(splitter).join(' ')) {
+      if      ('a' <= cp && cp <= 'z') { addr.push(cp); }
+      else if ('A' <= cp && cp <= 'Z') { addr.push(cp.toLowerCase()); }
+      else                             { addr.push('_'); }
+    }
+    return addr;
+  }
+}
+
 (function () {
   const resultsContainer = document.getElementById("results");
   const searchBox = document.getElementById("searchBox");
